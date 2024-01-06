aa8e9e08d84f5d46c5c487e3efd6fadac963577a
W298 <lutca1320@gmail.com>
2024-01-06 23:24:49 +0900

    Test with IOCP

--- main.cpp
+++ main.cpp
@@ -1,104 +1,86 @@
 #include <iostream>
-#include <string>
-#include <queue>
 #include <windows.h>
 
-using namespace std;
+#include "cvmarkersobj.h"
+#include "Helper.h"
+#include "Types.h"
 
-enum THREAD_TYPE
+#define THREAD_COUNT 8
+
+using namespace Concurrency::diagnostic;
+
+HANDLE g_threadHandleAry[THREAD_COUNT];
+HANDLE g_iocp;
+HANDLE g_threadIocpAry[THREAD_COUNT];
+
+DWORD WINAPI ThreadFunc(LPVOID param)
 {
-	GENERAL_NON_OVERLAP,
-	GENERAL,
-	CALL_ONLY,
-	COMPLETION_ONLY,
-	COMPUTE_ONLY,
-	CALL_AND_COMPLETION,
-	COMPLETION_AND_COMPUTE
-};
+	const HANDLE iocpHandle = *(HANDLE*)param;
+	marker_series threadSeries(_T("Thread"));
 
-enum THREAD_TASK_TYPE
-{
-	THREAD_TASK_READ_CALL,
-	THREAD_TASK_READ_COMPLETION,
-	THREAD_TASK_COMPUTE
-};
+	DWORD ret;
+	ULONG_PTR key;
+	LPOVERLAPPED lpov;
 
-struct ReadCallTaskArgs
-{
-	HANDLE fileHandle;
-	size_t byteSizeToRead;
-};
+	GetQueuedCompletionStatus(iocpHandle, &ret, &key, &lpov, INFINITE);
 
-void ReadCallTask(LPVOID args)
-{
-	ReadCallTaskArgs* a = (ReadCallTaskArgs*)(args);
+	threadSeries.write_flag(_T("Compute Request 01"));
+	ReadCallTaskArgs* readCallArgs = static_cast<ReadCallTaskArgs*>(lpov->Pointer);
+		readCallArgs->FileHandle = (HANDLE)20;
 
-	cout << a->byteSizeToRead << "\n";
-}
-
-struct ThreadTask
-{
-	THREAD_TASK_TYPE taskType;
-	LPVOID args;
-};
-
-class Thread
-{
-public:
-	queue<ThreadTask> taskQueue;
-
-	void ExecuteTask()
+	float number = 1.5;
+	while (true)
 	{
-		ThreadTask t = taskQueue.front();
-
-		switch (t.taskType)
-		{
-		case THREAD_TASK_READ_CALL:
-			ReadCallTask(t.args);
-			break;
-		}
+		number *= number;
 	}
-};
 
-UINT g_threadCount = 8;
-
-HANDLE CreateFileHandle(const string& fileName)
-{
-	return CreateFileA(fileName.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
-}
-
-void DummyFileCreation(const string& fileName, size_t byteSize)
-{
-	string command = "fsutil file createnew " + fileName + " " + to_string(byteSize);
-	system(command.c_str());
-}
-
-void DeleteDummyFiles()
-{
-	string command = "del dum*";
-	system(command.c_str());
-}
-
-void Clear()
-{
-	DeleteDummyFiles();
+	return 0;
 }
 
 int main()
 {
+	marker_series mainThreadSeries(_T("Main Thread"));
+
 	DummyFileCreation("dum", 2048);
 
-	HANDLE h = CreateFileHandle("dum");
+	g_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, THREAD_COUNT);
 
-	Thread thread;
+	for (int t = 0; t < THREAD_COUNT; t++)
+	{
+		g_threadIocpAry[t] = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 1);
 
-	ReadCallTaskArgs a = { h, 2048 };
-	ThreadTask t1 = { THREAD_TASK_READ_CALL, &a };
+		DWORD tid;
+		const HANDLE tHandle = CreateThread(NULL, 0, ThreadFunc, &g_threadIocpAry[t], 0, &tid);
+		if (tHandle == NULL)
+		{
+			ExitProcess(3);
+		}
 
-	thread.taskQueue.push(t1);
-	thread.ExecuteTask();
-	
-	Clear();
+		g_threadHandleAry[t] = tHandle;
+	}
+
+	for (int t = 0; t < THREAD_COUNT; t++)
+	{
+		ReadCallTaskArgs* args = (ReadCallTaskArgs*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(ReadCallTaskArgs));
+		args->FileHandle = INVALID_HANDLE_VALUE;
+		args->FileName = L"dum";
+		args->ByteSizeToRead = t;
+
+		OVERLAPPED ov;
+		ov.Pointer = args;
+
+		mainThreadSeries.write_flag(_T("Send Request"));
+		PostQueuedCompletionStatus(g_threadIocpAry[t], sizeof(ReadCallTaskArgs), THREAD_TASK_READ_CALL, &ov);
+	}
+
+	for (int t = 0; t < THREAD_COUNT; t++)
+	{
+		WaitForSingleObject(g_threadHandleAry[t], INFINITE);
+		CloseHandle(g_threadIocpAry[t]);
+		CloseHandle(g_threadHandleAry[t]);
+	}
+
+	DeleteDummyFiles();
 
 	return 0;
 }
